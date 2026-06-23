ac6cfa8e0dfd1266d34ef25e256b6c6c55204089
Ali Cantürk <alicanturk@Alis-MacBook-Pro.local>
2026-06-23 20:26:51 +0300

    Ana menü, tabbar ekledi, siğer iki panel sayfaları oluşturuldu

--- Assets/_MainMenu/Scripts/UIManager.cs
+++ Assets/_MainMenu/Scripts/UIManager.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    
+    void Start()
+    {
+        // Eğer 3D Saha sahnesi halihazırda yüklü değilse, arka plana yükle
+        if (!SceneManager.GetSceneByName("3d_Saha_Studio").isLoaded)
+        {
+            SceneManager.LoadScene("3d_Saha_Studio", LoadSceneMode.Additive);
+        }
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void OnPlayButtonPressed ()
     {
