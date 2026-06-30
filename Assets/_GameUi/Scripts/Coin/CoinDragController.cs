143c672f95caf49397e4c6a8d30a9ed12ed3ad0b
Ali Cantürk <alicanturk@Alis-MacBook-Pro.local>
2026-06-30 16:46:18 +0300

    Coin collider güncellendi

--- Assets/_GameUi/Scripts/Coin/CoinDragController.cs
+++ Assets/_GameUi/Scripts/Coin/CoinDragController.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(CoinAimIndicator))]
-[RequireComponent(typeof(CoinColliderFitter))]
 [RequireComponent(typeof(CoinIdentity))]
 [RequireComponent(typeof(CoinVisualState))]
 public class CoinDragController : MonoBehaviour
